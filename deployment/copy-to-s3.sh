b30f966a13e2c241b50db64ff271266af5c33a52
Aiden Dai  <daixb@amazon.com>
2020-10-14 14:40:21 +0800

    <chore> Update region list in copy-to-s3 script

--- deployment/copy-to-s3.sh
+++ deployment/copy-to-s3.sh
@@ -3,7 +3,7 @@
 # This script is used to upload cfn template and all regional assets to S3 Global partition 
 #
 # Extend this list as required
-region_list='us-west-2 us-east-1 us-east-2 us-west-1 eu-west-3 eu-west-1 eu-west-2 ap-southeast-1 ap-northeast-1 eu-central-1'
+region_list='ap-east-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2'
 
 echo "------------------------------------------------------------------------------"
 echo "[Init] Create all buckets in S3"
@@ -27,11 +27,11 @@ echo "[Upload] Upload assets to S3"
 echo "------------------------------------------------------------------------------"
 echo "copy global assets to region us-west-2"
 # aws s3 rm s3://$DIST_OUTPUT_BUCKET --recursive
-aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control --delete
+aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --acl public-read --delete 
 
 for r in $region_list
   do
    echo "copy regional assets to region $r"
    # aws s3 rm s3://$DIST_OUTPUT_BUCKET-$r --recursive
-   aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$r/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control --delete
+   aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$r/$SOLUTION_NAME/$VERSION/ --acl public-read --delete
 done
