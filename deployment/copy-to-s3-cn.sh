bb70522fcee165599142e15286b2a72b68aefa99
Aiden Dai  <daixb@amazon.com>
2020-10-15 10:11:03 +0800

    <chore> Update to use public-read for uploading to cn buckets

--- deployment/copy-to-s3-cn.sh
+++ deployment/copy-to-s3-cn.sh
@@ -27,6 +27,6 @@ for r in $region_list
   do
    echo "copy assets to region $r"
    #  aws s3 rm s3://$DIST_OUTPUT_BUCKET-$r --recursive
-   aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$r/$SOLUTION_NAME/$VERSION/ --delete --acl bucket-owner-full-control
-   aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$r/$SOLUTION_NAME/$VERSION/ --delete --acl bucket-owner-full-control
+   aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$r/$SOLUTION_NAME/$VERSION/ --acl public-read
+   aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$r/$SOLUTION_NAME/$VERSION/ --delete --acl public-read
 done
